55d4f83d2b7d3d3cd33e0629942736095794fb40
Ritik Arora <ritik@turings.xyz>
2023-04-14 03:06:56 +0530

    3rd redeploy

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import  Express  from "express";
 const PORT=3000;
 const app= Express();
 
-app.get("/", (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.json({ message: "Hello from server!" });
-});
-
-app.get("/users", (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.json({ message: "Hello from user!" });
-});
-
 //app is listening to the port
 app.listen(PORT,function(){
-    console.log(`listening at http://localhost:${PORT}`);
+  console.log(`listening at http://localhost:${PORT}`);
 })
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+
+// Export the Express API
+export default app;
+
